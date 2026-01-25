4d1027af354792c949ce3c81e6c71172c532bb1c
Asiegbu Miracle Kanu-Asiegbu <akanu@umich.edu>
2026-01-25 18:17:13 -0500

    Update Docker folder

--- docker/run.sh
+++ docker/run.sh
@@ -4,7 +4,6 @@ else
     GPU=$1
 fi
 
-#in gerneral do only read mode but since cache created need to write 
 docker run -it \
   -u $(id -u):$(id -g) \
   --rm \
@@ -12,14 +11,7 @@ docker run -it \
   --hostname $(hostname) \
   -e HOME \
   -v /mnt/roahm:/mnt/roahm \
-  -v /mnt/workspace/datasets:/mnt/workspace/datasets \
-  -v /mnt/roahm/users/akanu/projects/fusion_therm_rgb/state-of-art-detectors/yolov5:/home/yolov5 \
-  -v /home/akanu/akanu/projects/Projects_to_make_public/MambaST:/home/MambaST \
-  -v /mnt/ws-frb/projects/thermal_rgb/njotwani/multispectral-video-object-detection/runs/:/home/nitin/ \
-  -w /home/akanu \
+  -w /home/PUT_NAME_HERE \
   --ipc "host"\
-  -v $(pwd):/home/akanu \
+  -v $(pwd):/home/PUT_NAME_HERE \
    multispectral-obj-det:latest
-
-#   -v /mnt/workspace/users/vsrikar/DeformableDetr/data:/mnt/workspace/users/vsrikar/DeformableDetr/data \
-  # -v /home/akanu/akanu/local_datasets:/mnt/workspace/datasets \
